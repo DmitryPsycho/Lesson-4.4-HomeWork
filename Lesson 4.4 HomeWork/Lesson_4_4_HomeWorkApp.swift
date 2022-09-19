d1bd5ed41ed88cce157573c9f89d1dcb2e1b438d
DmitryPsycho <kovalenko2511@bk.ru>
2022-09-19 14:58:48 +0300

    Initial Commit

---
+++ Lesson 4.4 HomeWork/Lesson_4_4_HomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_4_HomeWorkApp.swift
+//  Lesson 4.4 HomeWork
+//
+//  Created by Psycho on 19.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_4_HomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
